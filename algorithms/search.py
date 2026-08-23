d419c1b1ce23769560ccf922f30bb93e2ae15f85
Tu nombre <aperez@grupotci.com.co>
2026-08-23 09:03:59 -0500

    Punto 2 y Punto 3

--- algorithms/search.py
+++ algorithms/search.py
@@ -37,7 +37,25 @@ def breadthFirstSearch(problem: SearchProblem):
     Search the shallowest nodes in the search tree first.
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    frontier = utils.Queue()
+
+    start_state = problem.getStartState()
+    frontier.push((start_state, []))
+
+    visited = {start_state}
+
+    while not frontier.isEmpty():
+        state, actions = frontier.pop()
+
+        if problem.isGoalState(state):
+            return actions
+
+        for successor, action, step_cost in problem.getSuccessors(state):
+            if successor not in visited:
+                visited.add(successor)
+                frontier.push((successor, actions + [action]))
+
+    return []
 
 
 def uniformCostSearch(problem: SearchProblem):
@@ -46,7 +64,40 @@ def uniformCostSearch(problem: SearchProblem):
     """
 
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    frontier = utils.PriorityQueue()
+
+    start_state = problem.getStartState()
+    frontier.push((start_state, [], 0), 0)
+
+    best_cost = {
+        start_state: 0
+    }
+
+    while not frontier.isEmpty():
+        state, actions, cost = frontier.pop()
+
+        if problem.isGoalState(state):
+            return actions
+
+        # Ignore outdated entries in the priority queue.
+        if cost > best_cost[state]:
+            continue
+
+        for successor, action, step_cost in problem.getSuccessors(state):
+            new_cost = cost + step_cost
+
+            if successor not in best_cost or new_cost < best_cost[successor]:
+                best_cost[successor] = new_cost
+
+                new_actions = actions + [action]
+
+                frontier.push(
+                    (successor, new_actions, new_cost),
+                    new_cost
+                )
+
+    return []
+
 
 
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
