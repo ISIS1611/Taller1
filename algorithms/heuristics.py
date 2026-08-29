457a3818e892e3edcaecb4f33b942a2653eaf1ce
npb868 <n.pineres@uniandes.edu.co>
2026-08-28 21:29:40 -0500

    Punto 5 (Se añadió A* y las heuristicas :P)

--- algorithms/heuristics.py
+++ algorithms/heuristics.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from algorithms import utils
 from algorithms.problems import SystemRepairProblem
+import math
 
 
 def nullHeuristic(state, problem=None):
@@ -22,7 +23,26 @@ def manhattanHeuristic(state, problem):
     - C if all systems have been repaired.
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    position, gotKit, systems = state
+    
+    if not gotKit:                      #Consigió el kit?
+        target = problem.kitPosition    # Si si, sigue; sino, no. Entonces ve para el kit
+    elif len(systems)>0:                # Reparó todo?, NO, entonces calcula:
+        closestSystem = systems[0]                                                                      #Toma el primer sistema
+        closestDistance = abs(position[0] - closestSystem[0]) + abs(position[1] - closestSystem[1])     # Calcula su distancia manhattan
+        
+        for system in systems:                                                                          #Para cada sistema, 
+            distance = abs(position[0] - system[0]) + abs(position[1] - system[1])        # Calcula su distancia manhattan
+            
+            if distance < closestDistance:                                                              #Busca la distancia mínima
+                closestDistance = distance
+                closestSystem = system
+                
+        target = closestSystem                                                                          # El objetivo es la distancia mínima
+    else:                               # SI, Ya reparó todo
+        target = problem.controlPosition    # Manda al robot a la meta final
+    
+    return abs(position[0] - target[0]) + abs(position[1] - target[1])      #Retorna la distancia manhattan entre la posición actual y el objetivo de posición.
 
 
 def euclideanHeuristic(state, problem):
@@ -36,7 +56,26 @@ def euclideanHeuristic(state, problem):
     - C if all systems have been repaired.
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    position, gotKit, systems = state
+    if not gotKit:                      #Consigió el kit?
+        target = problem.kitPosition    # Si si, sigue; sino, no. Entonces ve para el kit
+    elif len(systems)>0:                # Reparó todo?, NO, entonces calcula:
+        closestSystem = systems[0]                                                                                  #Toma el primer sistema
+        closestDistance = math.sqrt((position[0] - closestSystem[0])**2 + (position[1] - closestSystem[1])**2)      # Calcula su distancia euclidiana
+            
+        for system in systems:                                                                                      #Para cada sistema, 
+            distance = math.sqrt((position[0] - system[0])**2 + (position[1] - system[1])**2)         # Calcula su distancia euclidiana
+                
+            if distance < closestDistance:                                                              #Busca la distancia mínima
+                closestDistance = distance
+                closestSystem = system
+                    
+        target = closestSystem                                                                          # El objetivo es la distancia mínima
+    else:                               # SI, Ya reparó todo
+        target = problem.controlPosition    # Manda al robot a la meta final
+        
+    return math.sqrt((position[0] - target[0])**2 + (position[1] - target[1])**2)      #Retorna la distancia euclidiana entre la posición actual y el objetivo de posición.
+
 
 
 def systemRepairHeuristic(
@@ -57,4 +96,41 @@ def systemRepairHeuristic(
     - Balance heuristic strength vs. computation time (do experiments!)
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    position, gotKit, systems = state
+    
+    if not gotKit:                      # Si no ha conseguido el kit, tiene que ir por el, por lo que retorna la distancia manhattan hasta K desde R
+        kit = problem.kitPosition       #Toma ubicación de Kit
+        return abs(position[0] - kit[0]) + abs(position[1] - kit[1])    #Calcula posición manhattan entre el kit y el robot
+    
+    if len(systems) > 0:                # Aún hay sistemas por reparar?
+        nodes = [position]             # Crea una lista que servirá como MST. Guarda la posición actual del robot
+        
+        for system in systems:          # Para cada sistema que falta reparar
+            nodes.append(system)       # Lo añade a la lista de nodos a visitar
+            
+        nodes.append(problem.controlPosition)       # Añade a la lista el centro de control a visitar (C)
+        
+        included = [False] * len(nodes)             # Crea una nueva lista para saber cuales nodos ya est+an en el MST
+        included[0] = True                          # Marca la posición actual del robot
+        
+        totalCost = 0                               # Costo total del MST
+        for i in range(len(nodes) - 1):             # Repite hasta conectar todos los nodos
+            shortestDistance = float("inf")         # valores para hallar una distancia minima y un punto cercano
+            closestPoint = -1
+            
+            for j in range(len(nodes)):             # recorre todos los puntos
+                if included[j]:                     # Está incluido el nodo en el MST?
+                    for k in range(len(nodes)):     # Recorre los nodos que podrían conectarse
+                        if not included[k]:         # Este punto aún no está incluido?
+                            distance = (abs(nodes[j][0] - nodes[k][0])+ abs(nodes[j][1] - nodes[k][1])) #Manhatan entre ambos puntos (J y K)
+                            if distance < shortestDistance:     # Revisa si la distancia es la mínima y guarda la distancia y el punto
+                                shortestDistance = distance
+                                closestPoint = k
+                                
+            totalCost += shortestDistance       # Suma la distancia más corta al costo total
+            included[closestPoint] = True       # Marca que ya se visitó el nodo en el MST
+            
+        return totalCost                        #Retorna el Costo total para visitar todos los T y C
+    
+    control = problem.controlPosition           # Si ya había agarrado el Kit, y ya había revisado todos los T, entonces solo falta C
+    return abs(position[0] - control[0]) + abs(position[1] - control[1])    #Retorna Manhattan desde posición actual hasta C
--- algorithms/search.py
+++ algorithms/search.py
@@ -112,7 +112,33 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     Search the node that has the lowest combined cost and heuristic first.
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    
+    start_state = problem.getStartState()                   # Toma el estado inicial del problema
+    frontier = utils.PriorityQueue()                        # Crea una cola de prioridad vacia como frontera
+    
+    start_priority = heuristic(start_state, problem)        # Calcula g(n)+h(n) para determinar prioridad
+    frontier.push((start_state, [], 0), start_priority)     # Guarda el estado, las acciones realizadas y el costo acumulado
+                                                            #Estructura de la priority queue: (elemento:(estado[0], acciones[1], costo[2]), prioriodad[3])
+    best_cost = {start_state: 0}                            # Estado inicial con costo 0 (servirá para avanzar más adelante)
+    
+    while not frontier.isEmpty():                           # Explora mientras existan nodos por explorar de la frontera actual
+        state, actions, cost = frontier.pop()               # Saca de la frontera el nodo con menor prioridad (g(n)+h(n)) y sus estados, el de con menor prioridad
+        
+        if problem.isGoalState(state):                      # Revisa si llegó a la meta, y si si, devuelve el camino
+            return actions
+        
+        if cost>best_cost[state]:                           # Revisa el costo del nodo que recien sacó de la frontera, si es viable, continua ejecutando, sino para y pasa a siguiente nodo
+            continue
+        
+        for succesor, action, step_cost in problem.getSuccessors(state):        # Obtiene de los sucesores del nodo, su costo y acciones
+            new_cost = cost + step_cost                                         # Calcula el costo acumulado (suma el actual con el del sucesor)
+            if (succesor not in best_cost) or new_cost<best_cost[succesor]:     # Comprueba si es la primera vez que se visita (con el costo) o si hay ruta más barata
+                best_cost[succesor] = new_cost                                  # Si paso el if, significa que tiene mejor costo en ese estado, por lo que guarda el costo nuevo
+                new_actions = actions + [action]                                # Agrega la acción a la ruta tomada hasta el momento
+                priority = new_cost + heuristic(succesor,problem)               # Calcula el g(n)+h(n)
+                
+                frontier.push((succesor, new_actions, new_cost), priority)      # Agrega el nodo a la frontera, guardando toda su info
+    return []   # Si falló ps no retorna nadota
 
 
 # Abbreviations (you can use them for the -f option in main.py)
